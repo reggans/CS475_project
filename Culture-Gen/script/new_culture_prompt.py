127cc63cd1d4049e75dad6f7ec1e4ed3dc31e92d
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2024-11-30 11:19:36 +0900

    bugfix

--- Culture-Gen/script/new_culture_prompt.py
+++ Culture-Gen/script/new_culture_prompt.py
@@ -179,7 +179,7 @@ def prompting_pipeline(
 
                                     prompt = opinion_prompt + prompt
                                     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-                                    outputs.extend([_ for _ in 
+                                    outputs = torch.cat([outputs,
                                                     model.generate(**inputs, do_sample=True, num_return_sequences=10, max_new_tokens=30, top_p=1, top_k=50, pad_token_id=tokenizer.eos_token_id)])
                             else:
                                 outputs = model.generate(**inputs, do_sample=True, num_return_sequences=n_sample//10, max_new_tokens=30, top_p=1, top_k=50, pad_token_id=tokenizer.eos_token_id)
